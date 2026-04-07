cf10f848c2c9d5140e429abaa5b5ab76d7d52466
ignac8 <995943+ignac8@users.noreply.github.com>
2026-04-07 23:23:25 +0200

    Remove redundant automa check in constructor (constructorActivePlayer is always valid)

--- src/server/Game.ts
+++ src/server/Game.ts
@@ -215,7 +215,7 @@ export class Game implements IGame, Logger {
     if (playerIds.includes(first.id) === false) {
       throw new Error('Cannot find first player ' + first.id + ' in [' + playerIds + ']');
     }
-    if (playerIds.includes(activePlayer) === false && !gameOptions.automaOption) {
+    if (playerIds.includes(activePlayer) === false) {
       throw new Error('Cannot find active player ' + activePlayer + ' in [' + playerIds + ']');
     }
     if (new Set(playerIds).size !== players.length) {
