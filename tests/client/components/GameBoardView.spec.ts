1238651883292f3c6b443ee8238d4a8053cf8c76
ignac8 <995943+ignac8@users.noreply.github.com>
2026-05-15 00:11:07 +0200

    GameBoardView: test marsBot rendering and isAutoma flag

--- tests/client/components/GameBoardView.spec.ts
+++ tests/client/components/GameBoardView.spec.ts
@@ -2,6 +2,10 @@ import {shallowMount} from '@vue/test-utils';
 import {expect} from 'chai';
 import {globalConfig} from './getLocalVue';
 import GameBoardView from '@/client/components/GameBoardView.vue';
+import MarsBotPanel from '@/client/components/automa/MarsBotPanel.vue';
+import Milestones from '@/client/components/Milestones.vue';
+import Awards from '@/client/components/Awards.vue';
+import {MarsBotModel} from '@/common/models/MarsBotModel';
 import {fakeGameModel} from './testHelpers';
 
 describe('GameBoardView', () => {
@@ -15,5 +19,33 @@ describe('GameBoardView', () => {
       },
     });
     expect(wrapper.exists()).to.be.true;
+  });
+
+  it('does not render MarsBotPanel when game.marsBot is undefined', () => {
+    const wrapper = shallowMount(GameBoardView, {
+      ...globalConfig,
+      props: {
+        game: fakeGameModel(),
+        tileView: 'show',
+        playerCount: 2,
+      },
+    });
+    expect(wrapper.findComponent(MarsBotPanel).exists()).to.be.false;
+    expect(wrapper.findComponent(Milestones).props('isAutoma')).to.be.false;
+    expect(wrapper.findComponent(Awards).props('isAutoma')).to.be.false;
+  });
+
+  it('renders MarsBotPanel and flags isAutoma when game.marsBot is set', () => {
+    const wrapper = shallowMount(GameBoardView, {
+      ...globalConfig,
+      props: {
+        game: fakeGameModel({marsBot: {} as MarsBotModel}),
+        tileView: 'show',
+        playerCount: 1,
+      },
+    });
+    expect(wrapper.findComponent(MarsBotPanel).exists()).to.be.true;
+    expect(wrapper.findComponent(Milestones).props('isAutoma')).to.be.true;
+    expect(wrapper.findComponent(Awards).props('isAutoma')).to.be.true;
   });
 });
